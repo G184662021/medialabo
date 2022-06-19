086f56df9e7d8cdb37851fa24c1e8a23407a3340
G184662021 <218466@st.takushoku-u.ac.jp>
2022-06-19 14:30:48 +0900

    main

--- project-js/kazuate.js
+++ project-js/kazuate.js
@@ -8,14 +8,25 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
+
 hantei();
 hantei();
 hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+function hantei(number) {
+    let yoso = 4;
+    // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
+    if(kaitou === yoso){
+        console.log('正解です．おめでとう!');
+    }
+    else if(kaitou < yoso){
+        console.log('まちがい．答えはもっと小さいですよ');
+    }
+    else if(kaitou > yoso){
+        console.log('まちがい．答えはもっと大きいですよ');
+    }
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
 
