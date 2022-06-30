ec723f5208990ce0c6129fb8811e519df52098d7
G184662021 <218466@st.takushoku-u.ac.jp>
2022-06-30 12:29:16 +0900

    main

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -9,3 +9,30 @@ li = document.createElement('li');
 li.textContent = 'ヨーロッパ大陸';
 ul.insertAdjacentElement('beforeend',li)
 
+let h2sevenOcean = document.querySelector('h2#savenOcean')
+
+// 7大洋　の名前を配列に代入
+let oceans = [
+  '北極海',
+  '北大西洋',
+  '南大西洋',
+  'インド洋',
+  '北太平洋',
+  '南太平洋',
+  '南極海'
+]
+
+// ul 要素を新規作成する．（次の行を完成させる）
+ul = document.createElement('ul');
+
+// ul 要素を h2sevenOcean の次に追加する（次の行を書く）
+
+ul.insertAdjacentElement('afterend',ul);
+// ul に7つの大陸を追加する
+for (let o of oceans) {
+    li = document.createElement('li');
+    li.textContent = oceans[o];
+    li.insertAdjacentElement('beforeend',li);
+
+}
+
