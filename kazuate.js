7d0ae8390437093935da0c9056357dfee045ed0f
G184662021 <218466@st.takushoku-u.ac.jp>
2022-07-07 12:06:05 +0900

    main

--- kazuate.js
+++ kazuate.js
@@ -7,15 +7,14 @@ let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
-hantei();
-hantei();
-hantei();
-
+let b2 = document.querySelector('button#print');
+b2.addEventListener('click', hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    let yoso = 4;
+   
     // 第5回課題:テキストボックスの数値をここに代入
+    let i = document.querySelector('input[name="nyuryoku"]');
+    let yoso = i.value;
     // 課題3-1：ここの判定処理を作成する．
     let a = document.querySelector('span#result')
     let b = document.querySelector('span#answer')
@@ -29,6 +28,8 @@ function hantei() {
     else if(kotae > yoso){
         a.textContent='まちがい．答えはもっと大きいですよ';
     }
+
+
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
 
