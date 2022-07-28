5b544cfa4fb8d3aadbd1c044b5dfe8be307e2603
G184662021 <218466@st.takushoku-u.ac.jp>
2022-07-28 09:13:35 +0900

    main

--- weather.js
+++ weather.js
@@ -1,12 +1,3 @@
-// #page-topをクリックした際の設定
-$('#page-top').click(function () {
-  $('body,html').animate({
-      scrollTop: 0//ページトップまでスクロール
-  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-  return false;//リンク自体の無効化
-});
-
-
 let b = document.querySelector('button#push');
 b.addEventListener('click', search)
 
