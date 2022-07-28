207e490ed24760b7a85c846d085b9c32ab500ef1
G184662021 <218466@st.takushoku-u.ac.jp>
2022-07-28 11:10:35 +0900

    main

--- weather.js
+++ weather.js
@@ -2,153 +2,112 @@ let ba = document.querySelector("span#basyo");
 let hk = document.querySelector("span#highkion");
 let lk = document.querySelector("span#lowkion");
 let si = document.querySelector("span#sitsudo");
-let huk = document.querySelector("span#huko");
-let hus = document.querySelector("span#husoku");
+let hus = document.querySelector("span#husoku")
 let img = document.createElement('img');
 let tenki =document.querySelector("span#tenki");
+let count=0;
+    let b = document.querySelector('button#push');
+    b.addEventListener('click', search);
 
-let b = document.querySelector('button#push');
-b.addEventListener('click', search)
-
+// 通信を開始する処理
 function search() {
-  var a=document.getElementById('search');
-  if(a.value ==="1") {
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/360630.json";
-    axios.get(url)
-    .then(showResult)  //通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);  
-  }else if(a.value ==="2" ){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/524901.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value=== "3"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/993800.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value === "4"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/1816670.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);  
-  }else if(a.value === "5"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/1850147.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value === "6"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/1880252.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value === "7"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/2147714.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value=== "8"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/2643743.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value === "9"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/2968815.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value === "10"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/3451189.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value === "11"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/5128581.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else if(a.value ==="12"){
-    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/5368361.json";
-    axios.get(url)
-    .then(showResult)   // 通信成功
-    .catch(showError)   // 通信失敗
-    .then(finish);
-  }else {
-    let url =null;
-    
+  console.log(count);    
+  let math;
+  let x;
+      let y=document.querySelectorAll('input[name="push"]');
+      for(math of y){
+          if(math.checked){
+              x=math.value;
+          }
+      }
+
+      // URL を設定
+      let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+x+'.json';
+
+      // 通信開始
+      axios.get(url)
+          .then(showResult)   // 通信成功
+          .catch(showError)   // 通信失敗
+          .then(finish);      // 通信の最後の処理
   }
 
-}
 
+  // 通信が成功した時の処理
+  function showResult(resp) {
+      count+=1;
+      // サーバから送られてきたデータを出力
+      let data = resp.data;
 
+      // data が文字列型なら，オブジェクトに変換する
+      if (typeof data === 'string') {
+          data = JSON.parse(data);
+      }
 
-// 通信が成功した時の処理
-function showResult(resp) {
-    // サーバから送られてきたデータを出力
-    let data = resp.data;
+      let a;
+      let toshi=document.querySelector('div#tosi');
+      if(data.name==="State of Rio de Janeiro"){
+          a="リオデジャネイロ";
+      }else if(data.name==="Paris"){
+          a="パリ";
+      }else{
+          a=data.name;
+      }
+      toshi.textContent='都市名:'+a;
+      
 
-    // data が文字列型なら，オブジェクトに変換する
-    if (typeof data === 'string') {
-        data = JSON.parse(data);
-    }
-    let te =data.weather[0].description;
-    tenki.textContent =data.weather[0].description;
-    ba.textContent =data.name;
-    hk.textContent =data.main.temp_max+"℃";
-    lk.textContent =data.main.temp_min+"℃";
-    si.textContent =data.main.humidity+"%";
-    huk.textContent =data.wind.deg;
-    hus.textContent =data.wind.speed+"m/s";
-   
-    let sun = "tennki-illust2.png";
-    let cloud = "tennki-illust6.png";
-    let rain = "tennki-illust7.png";
-    let kiri = "yama_kiri.png";
-    if (te==='晴天'){
+      let iremono;
+      let r;
+      let ul;
+      let li;
+      let i=document.querySelectorAll('input[name="know"]');
+          ul=document.createElement("ul");
+          toshi.insertAdjacentElement("afterend",ul);
 
-      img.setAttribute('src', sun);
-      img.setAttribute('width', '300px');
-      img.setAttribute('height', 'auto');
-      img.setAttribute('margin', 'auto');
-      let picture = document.querySelector("div#weather");
-      picture.insertAdjacentElement("beforeend", img);
-    }else if (te==="厚い雲"||te==="雲"||te==="曇りがち"){
+      for(r of i){
+          if(r.checked){
+              li=document.createElement("li");
+              iremono=r.value;
+              if(iremono==='weather.description'){
+                  li.textContent='天気:'+ data.weather[0].description;
+                  ul.insertAdjacentElement("beforeend",li);
+                  if(data.weather[0].main==="Clear"){
+                      ul.style.backgroundColor="rgba(2, 175, 255, 0.373)";
+                  }else if(data.weather[0].main==="Clouds"){
+                      ul.style.backgroundColor="rgba(177, 174, 197, 0.575)";
+                  }else if(data.weather[0].main==="Rain"){
+                      ul.style.backgroundColor="rgba(81, 148, 255, 0.495)";
+                  }else if(data.weather[0].main==="Mist"){
+                      ul.style.backgroundColor="rgba(190, 220, 255, 0.495)";
+                  }
+              }
+              if(iremono==='main.humidity'){
+                  li.textContent='湿度:'+ data.main.humidity+"%";
+                  ul.insertAdjacentElement("beforeend",li);
+              }
+              if(iremono==='main.temp_max'){
+                  li.textContent='最高気温:'+ data.main.temp_max+"℃";
+                  ul.insertAdjacentElement("beforeend",li);
+              }
+              if(iremono==='main.temp_min'){
+                  li.textContent='最低気温:'+ data.main.temp_min+"℃";
+                  ul.insertAdjacentElement("beforeend",li);
+              }
+              if(iremono==='wind.speed'){
+                  li.textContent='風速:'+ data.wind.speed+"m/s";
+                  ul.insertAdjacentElement("beforeend",li);
+              }
+          if(iremono==='feels_like'){
+              li.textContent='体感温度:'+data.feels_like+"℃";
+              ul.insertAdjacentElement("beforeend",li);
+          }
+          }
+      }if(count>=1){
+          let p=document.createElement("p");
+          p.textContent=a+"の検索結果です  ⤴";
+          ul.insertAdjacentElement("afterend",p);
+      }
+  }
 
-      img.setAttribute('src', cloud);
-      img.setAttribute('width', '300px');
-      img.setAttribute('height', 'auto');
-      img.setAttribute('margin', 'auto');
-      let picture = document.querySelector("div#weather");
-      picture.insertAdjacentElement("beforeend", img);
-    }else if (te==="小雨") {
-
-      img.setAttribute('src', rain);
-      img.setAttribute('width', '300px');
-      img.setAttribute('height', 'auto');
-      img.setAttribute('margin', 'auto');
-      let picture = document.querySelector("div#weather");
-      picture.insertAdjacentElement("beforeend", img);
-    }else if (te==="霧") {
-  
-      img.setAttribute('src', kiri);
-      img.setAttribute('width', '300px');
-      img.setAttribute('height', 'auto');
-      img.setAttribute('margin', 'auto');
-      let picture = document.querySelector("div#weather");
-      picture.insertAdjacentElement("beforeend", img);
-    }
-}
 
 // 通信エラーが発生した時の処理
 function showError(err) {
