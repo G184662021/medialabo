3f7964695c34d9a1599b1b1d5227bb389e0f2d2d
G184662021 <218466@st.takushoku-u.ac.jp>
2023-11-27 12:27:35 +0900

    renda

--- renda/tapGame.js
+++ renda/tapGame.js
@@ -16,11 +16,9 @@ const startButton = document.getElementById("startButton");
 function startGame() {
   // ボタンの非表示
   startButton.style.display = "none";
-
-  // タップカウンターのリセットと表示
+  // スコアとタイマーのリセット
   counter = 0;
-  score.textContent = "スコア: 0";
-
+  updateScore();
   // タイマーリセット(カウントダウン＋10秒)
   countTimer = 13;
   // タイマーを起動
@@ -37,15 +35,19 @@ function countTime(time) {
     } else {
       tapFlag = true;
     }
+    // タイマーカウントを表示
+    countDown.textContent = time - 3;
     time -= 1;
     // １秒後にtimeを引数にしてcountTime()を呼び出す
     setTimeout(countTime, 1000, time);
   } else {
     // 時間切れの処理
     tapFlag = false;
-    countDown.textContent = "タイムアップ！ スコア: " + counter;
+    countDown.textContent = "タイムアップ！";
     // ボタンの表示
     startButton.style.display = "block";
+    // スコアを表示
+    score.textContent = "スコア: " + counter;
   }
 }
 
@@ -53,7 +55,12 @@ function countTime(time) {
 function tapCount() {
   if (tapFlag) {
     counter += 1;
-    // タップ数とスコアを表示
-    score.textContent = "スコア: " + counter;
+    // タップ数を表示
+    updateScore();
   }
+}
+
+// スコア表示更新
+function updateScore() {
+  score.textContent = "スコア: " + counter;
 }
